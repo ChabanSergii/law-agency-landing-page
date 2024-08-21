ec18acc2fb86422771772eed295ac245dce39a62
chaban <chabchabik@gmail.com>
2024-08-21 16:39:59 +0300

    added jquery local

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", function() {
+    /* JQuery Progres Bar */
+    $( "#family" ).progressbar({
+        value: 75
+      });
+
+      $( "#construction" ).progressbar({
+        value: 80
+      });
+
+      $( "#finance" ).progressbar({
+        value: 75
+      });
+
+      $( "#accident" ).progressbar({
+        value: 90
+      });
+})
